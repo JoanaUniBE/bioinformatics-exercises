031bd4a94c8707513cdc09c46b69e5f7643ac3f1
Joana_Rechsteiner <joana.rechsteiner@unibe.ch>
2024-11-13 14:17:39 +0100

    Test stringent quality thresholds

--- dataProcess/scripts/fastp_clean.sh
+++ dataProcess/scripts/fastp_clean.sh
@@ -9,7 +9,7 @@
 
 module add fastp/0.23.4-GCC-10.3.0
 
-fastp -w 4 -q 15 -z 5 -l 50  -i SRR1027171_1.fastq.gz -I SRR1027171_2.fastq.gz -o SRR1027171_1.clean.fq.gz -O SRR1027171_2.clean.fq.gz
+fastp -w 4 -q 20 -z 5 -l 60  -i SRR1027171_1.fastq.gz -I SRR1027171_2.fastq.gz -o SRR1027171_1.clean.fq.gz -O SRR1027171_2.clean.fq.gz
 
 # Trim low-quality ends from reads before adapter removal if quality is less than 15 (-q 15)
 # Discard trimmed reads that are shorter than 50 bases after trimming (-l 50)
